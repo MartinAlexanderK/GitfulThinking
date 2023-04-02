2f69ed77078696d556646b76eee75725385b9340
Martin Kaeser <Ma.kaeser@gmail.com>
2023-04-02 10:53:08 +0200

    Toggles added

---
+++ SwiftfulThinkingBootcamp/ToggleBootCamp.swift
@@ -0,0 +1,34 @@
+//
+//  ToggleBootCamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Martin Kaeser on 02.04.23.
+//
+
+import SwiftUI
+
+struct ToggleBootCamp: View {
+    @State var isOn : Bool = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Status")
+                Text(isOn ? "online" : "offline")
+            }
+            .font(.title)
+            Toggle(isOn: $isOn) {
+                Text("Change Status")
+            }
+            .toggleStyle(SwitchToggleStyle(tint: Color.red))
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct ToggleBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ToggleBootCamp()
+    }
+}
